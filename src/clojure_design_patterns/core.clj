7824e0369521356b04f82a595bcbfab68dbbef15
ganeshneelekani <ganeshneelekani@gmail.com>
2023-02-12 20:16:23 +0530

    added some logic

--- src/clojure_design_patterns/core.clj
+++ src/clojure_design_patterns/core.clj
@@ -1,7 +1,25 @@
 (ns clojure-design-patterns.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;; Accessing the history of a `ref `in Clojure
+
+(def change (ref "hello" :min-history 10))
+
+(def history-of-change (atom [@change]))
+
+(add-watch change :watcher (fn [_ _ old-value new-value]
+                                (when (not= old-value new-value)
+                                  (swap! history-of-change conj new-value))))
+
+(do
+  (dosync (ref-set change "better"))
+  @change)
+
+(let [exclamator (fn [x] (str x "!"))]
+  (dosync
+   (alter change exclamator)
+   (alter change exclamator)
+   (alter change exclamator))
+  @change)
+
+(ref-history-count change)
